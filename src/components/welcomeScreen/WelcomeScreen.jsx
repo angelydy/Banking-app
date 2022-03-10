be790307a034904b2b62d2ff3d3d3aeded2db34a
angely <angelymaedyy@gmail.com>
2022-03-10 18:44:01 +0800

    second commit

--- src/components/welcomeScreen/WelcomeScreen.jsx
+++ src/components/welcomeScreen/WelcomeScreen.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import pic from './../../assets/02.png';
 import leftimg from './../../assets/business-woman.png';
+import { useState } from "react";
 import './WelcomeScreen.css';
 
 function WelcomeScreen() {
+  const [switchToggle, setSwitchToggle] = useState(false)
+
+  const buttonClicked = () => {
+    switchToggle ? setSwitchToggle(false) : setSwitchToggle(true)
+  };
+
   return (
     <div className="welcome">
       <navbar>
@@ -17,9 +24,9 @@ function WelcomeScreen() {
           <a>
             Blog
           </a>
-          <button className='SignUp'>
-            Sign Up
-          </button>
+            <button className="SignUp" onClick={buttonClicked}>
+              Sign Up
+            </button>
         </div>
       </navbar>
 
@@ -52,7 +59,7 @@ function WelcomeScreen() {
         </div>
       </div>
 
-      <div class="sign-up-modal">
+      <div className={switchToggle ? "show-sign-up-modal" : "sign-up-modal"}>
         <div class="form-wrapper">
           <div className="left-wrapper">
             <p className="left-wrapper-text">
@@ -63,19 +70,33 @@ function WelcomeScreen() {
             <img src={leftimg} className="modalIcon"></img>
           </div>
           <div className="right-wrapper">
-          <button id="close-sign-up">X</button>
+          <button id="close-sign-up" onClick={buttonClicked}>X</button>
           <form action="results.html" id="form1" method="get">
             <label for="fullname" class="fullname-label">Name</label>
-            <input type="name" id="first-name" placeholder="First Name" required></input>
-            <input type="name" id="last-name" placeholder="last Name" required></input>
-            <label for="email" class="email-label">Email</label>
-            <input type="email" id="email" placeholder="sy.feydaniel@gmail.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
-            <label for="mobile-number" class="mobile-number-label">Phone Number</label>
-            <input type="number" id="mobile-number" placeholder="Enter your mobile number here" required></input>
-            <label for="password" class="password-label">Password</label>
-            <input type="password" id="password" placeholder="Input a password" required></input>
+            <div className="name-container">
+              <input type="name" id="first-name" placeholder="First Name" required></input>
+              <input type="name" id="last-name" placeholder="Last Name" required></input>
+            </div>
+            <div className="email-and-accountNumber">
+              <div className="email-and-accountNumber-label">
+                <label for="email" class="email-label">Email</label>
+                <label for="account-number" class="account-number-label">Account No</label>
+              </div>
+              <input type="email" id="email" placeholder="Enter email here" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+              <input type="number" id="account-number" placeholder="Enter your account number here" required></input>
+            </div>  
+            <div className="username-password">
+              <div className="username-password-label">
+                <label for="password" class="password-label">Password</label>         
+                <label for="username" class="username-label">Username</label>         
+              </div>
+              <input type="password" id="password" placeholder="Password (8  or more characters)" required minLength={8}></input>
+              <input type="text" id="password" placeholder="Enter username" required></input>
+            </div> 
+            <input type="password" id="confirm-password" placeholder="Confirm password" required></input>
             <input type="submit" id="submitBtn" placeholder="Submit"></input>
           </form>
+
           <div class="disclaimer">By submitting this form, you agree to the Terms of Use and Privacy of this website.</div>
         </div></div>
       </div>
