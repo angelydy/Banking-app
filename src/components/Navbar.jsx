3a56b8e46db2ff8853d0b3bc75bd0cd52928c7c4
lkpallarca <kencepallarca@gmail.com>
2022-03-14 09:48:48 +0800

    Modified fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./../assets/favicon.ico";
 import './../css/index.css';
 
 export default function Navbar() {
-  const [path, setPath] = useState('/')
-
   return (
     <nav>
       <img src={logo} className="logo" />
       <div className="mode-and-log-out">
         <i className="fa-solid fa-moon"></i>
         <Link
-          to={path}
+          to='/'
         >
           <i className="fa-solid fa-arrow-right-from-bracket"></i>
         </Link>
