e4c37d50c62a907af8b10199d99eb7ba31002509
lkpallarca <kencepallarca@gmail.com>
2022-03-22 15:23:03 +0800

    Create initial budget app logic and minor fixes

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChildBalance from './ChildBalance';
 
 export default function UserInfo({ accessingUser, passedName, passedUserName, passedAccCateg, passedAccType, passedBalance, ifParent, passedChildAccNum, passedChildName, passedChildBalance, ifHasChildren }) {
   const today = new Date();
   const hrs24 = today.getHours();
+  const [userExpense, setUserExpense] = useState([])
+  const [expenseCost, setExpenseCost] = useState([])
+  let users = JSON.parse(localStorage.getItem("users"))
 
   function getHours(h) {
     if (h < 12) {
@@ -11,9 +14,29 @@ export default function UserInfo({ accessingUser, passedName, passedUserName, pa
     } else if (h <= 18) {
       return `Good Afternoon, ${passedUserName} 👋🏼`
     } else {
-      console.log(passedChildAccNum)
       return `Good Evening, ${passedUserName} 👋🏼`
     }
+  }
+
+  function handleExpenseInput(e) {
+    setUserExpense(e.target.value)
+  }
+
+  function handleExpenseCostInput(e) {
+    setExpenseCost(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    users.find(user => {
+      if(user.accNum == accessingUser) {
+        let newUserExpense = {item: userExpense, cost: expenseCost}
+        user.expenses.push(newUserExpense)
+        localStorage.setItem('users', JSON.stringify(users))
+        alert('congrats')
+      }
+    })
+    e.target.reset()
   }
 
   return (
@@ -30,14 +53,14 @@ export default function UserInfo({ accessingUser, passedName, passedUserName, pa
       <div className='curr-balance'>
         <p className='balance-title'>Your Balance</p>
         <p className='balance-val'>{passedBalance}</p>
-        <div className='curr-status'>
-          <p>Currency</p>
-          <p>Status</p>
-        </div>
-        <div className='curr-status-val'>
-          <p>PHP <span><img src="https://img.icons8.com/color/20/000000/philippines-circular.png"/></span></p>
-          <p>Active</p>
-        </div>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <label htmlFor="expense-item">Add Expense Item</label>
+          <input onChange={handleExpenseInput} type="text" name="expense-item" id="expense-item"/>
+          <label htmlFor="expense-item-cost">Item Cost</label>
+          <input onChange={handleExpenseCostInput} type="text" name="expense-item-cost" id="expense-item-cost"/>
+          <button>Add</button>
+          <button type='reset'>Reset</button>
+        </form>
       </div>
       {ifParent === true &&
         <ChildBalance 
